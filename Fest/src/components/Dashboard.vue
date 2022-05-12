06c25c7e2a76985e187cc31657e13327d77d2259
SantRV <santiagoreyes.training@gmail.com>
2022-05-12 18:14:18 +0930

    Ryan update

--- Fest/src/components/Dashboard.vue
+++ Fest/src/components/Dashboard.vue
@@ -1,8 +1,51 @@
 <template>
-    <h1>
-        Dashboard
-    </h1>
-    
+    <div class="dashboard-content-container">
+        <div class="dashboard-title-container">
+            <h1>
+                Dashboard - Your events
+            </h1>
+        </div>
+        <div class="dashboard-buttons-container">
+            <button type="button" class="new-event-button">
+                <div class="button-icon">
+                    <svg width="100%" height="100%" viewBox="0 0 2132 2107" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;"><rect id="Plus" x="0" y="-0" width="2131.59" height="2106.64" style="fill:none;"/><g><path d="M1117.5,236.357l-0,1633.93" style="fill:none;stroke:#fff;stroke-width:350.19px;"/><path d="M300.53,1053.32l1633.93,0" style="fill:none;stroke:#fff;stroke-width:350.19px;"/></g></svg>
+                </div>
+                New Event
+            </button>
+            <button type="button" class="edit-button">
+                <div class="button-icon">
+                    <svg width="100%" height="100%" viewBox="0 0 2132 2107" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;"><rect id="Plus" x="0" y="-0" width="2131.59" height="2106.64" style="fill:none;"/><g><path d="M1117.5,236.357l-0,1633.93" style="fill:none;stroke:#fff;stroke-width:350.19px;"/><path d="M300.53,1053.32l1633.93,0" style="fill:none;stroke:#fff;stroke-width:350.19px;"/></g></svg>
+                </div>
+                Edit
+            </button>
+        </div>
+
+        <!-- Posts container -->
+        <div class="posts-content-container">
+            <div class="post-container">
+                <div class="post-image-container">
+
+                </div>
+              
+                <div class="post-icon-container">
+                    <p>
+                        &#129409;
+                    </p>
+                </div>
+                <div class="post-details-container">
+                    <div class="post-title-container">
+                        <h3>
+                            Party at Adelaide Uni!
+                        </h3>
+                    </div>
+                    <p>
+                        Static website for the coming Fest Application made by the best PG group!
+                    </p>
+                </div>
+                
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
