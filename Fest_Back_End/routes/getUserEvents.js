039c5c2f0e3a316eb163d5a1fdc24c8c28ad2715
SantRV <santiagoreyes.training@gmail.com>
2022-05-23 23:02:11 +0930

    Get info from users SQL

---
+++ Fest_Back_End/routes/getUserEvents.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+
+    // Get all user events
+    req.db.getConnection(function(error, connection) {
+        if (error) {
+            res.sendStatus(500);
+            return;
+        }
+        
+        let query = 'Call GetUserEvents(?)';
+
+        connection.query(query, req.query.user_id, function(error, rows, fields) {
+            connection.release();
+            if(error){
+                res.sendStatus(500);
+                return;
+            }
+            res.json(rows);
+        })
+    })
+});
+
+module.exports = router;
