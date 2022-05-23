9d79bb8b3616e22de1f78aafbfc8b7f0f516be74
chobbiwan <nguyerya@gmail.com>
2022-05-23 08:03:46 +0000

    sql table

--- Fest_Database/test.sql
+++ Fest_Database/test.sql
@@ -2,7 +2,7 @@ CREATE DATABASE fest;
 SELECT fest;
 
 CREATE Table users (
-  user_id int,
+  user_id int NOT NULL,
   first_name varchar(63),
   last_name varchar(63),
   dob date,
@@ -14,11 +14,12 @@ CREATE Table users (
   [state] varchar(63),
   post_code int,
   is_admin boolean,
-  profile_picture varchar(255)
+  profile_picture varchar(255),
+  PRIMARY KEY (user_id)
 );
 
 CREATE Table event (
-  event_id int,
+  event_id int NOT NULL,
   event_start datetime,
   event_end datetime,
   event_discription varchar(510),
@@ -29,50 +30,56 @@ CREATE Table event (
   post_code varchar(20),
   icon varchar(20),
   is_active boolean,
-  event_type int
+  event_type int,
+  PRIMARY KEY (event_id)
 );
 
 CREATE Table event_types (
-  type_id int,
-  type_desc varchar(500)
+  type_id int NOT NULL,
+  type_desc varchar(500),
+  PRIMARY KEY(type_id)
 );
 
 CREATE Table images (
-  image_id int,
-  event_id int,
-  image_loc varchar(255)
+  image_id int NOT NULL,
+  event_id int NOT NULL,
+  image_loc varchar(255),
+  PRIMARY KEY (image_id)
 );
 
 CREATE Table attendance (
-  att_id int,
-  user_id int,
-  event_id int,
-  user_status int
+  att_id int NOT NULL,
+  user_id int NOT NULL,
+  event_id int NOT NULL,
+  user_status int,
+  PRIMARY KEY(att_id)
 );
 
 CREATE Table organiser (
-  org_id int,
-  event_id int,
-  user_id int
+  org_id int NOT NULL,
+  event_id int NOT NULL,
+  user_id int NOT NULL,
+  PRIMARY KEY(org_id)
 );
 
 CREATE Table user_logs (
-  log_id int,
-  user_id int,
-  date_time timestamp
+  log_id int NOT NULL,
+  user_id int NOT NULL,
+  date_time timestamp,
+  PRIMARY KEY (log_id)
 );
 
 CREATE Table notifications (
-  notification_id int,
-  user_id int,
-  type int,
-  created_at timestamp
+  notification_id int NOT NULL,
+  user_id int NOT NULL,
+  type_id int NOT NULL,
+  created_at timestamp,
+  PRIMARY KEY (notification_id)
 );
 
 CREATE Table notification_type (
-  type_id int,
-  type_desc varchar(100)
+  type_id int NOT NULL,
+  type_desc varchar(100),
+  PRIMARY KEY (type_id)
 );
 
-//uhdiusad
-//dd
