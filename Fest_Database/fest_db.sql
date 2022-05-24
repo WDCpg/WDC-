61bafc95c16a7be4231d66f166c1fa47642b8319
chobbiwan <nguyerya@gmail.com>
2022-05-24 08:59:06 +0000

    database updated

--- Fest_Database/fest_db.sql
+++ Fest_Database/fest_db.sql
@@ -22,18 +22,25 @@ CREATE Table users (
 CREATE Table friends (
   user_id int NOT NULL,
   friend_id int NOT NULL
+  FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+  FOREIGN KEY(friend_id) REFERENCES users(user_id) ON DELETE CASCADE
+
 );
 
 CREATE Table calendar_availability(
   calendar_id int NOT NULL,
   user_id int,
+  start_date datetime,
+  end_date datetime,
+  PRIMARY KEY (calendar_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 )
 
-CREATE Table event (
+CREATE Table events (
   event_id int NOT NULL AUTO_INCREMENT,
   event_start datetime,
   event_end datetime,
-  event_discription varchar(510),
+  event_description varchar(510),
   event_title varchar(100),
   country varchar(63),
   city varchar(63),
@@ -42,42 +49,47 @@ CREATE Table event (
   icon varchar(20),
   is_active boolean,
   event_type int,
-  PRIMARY KEY (event_id)
+  PRIMARY KEY (event_id),
+  FOREIGN KEY (event_type) REFERENCES event_types(type_id)
 );
 
 CREATE Table event_types (
   type_id int NOT NULL AUTO_INCREMENT,
   type_desc varchar(500),
-  PRIMARY KEY(type_id)
+  PRIMARY KEY(type_id),
 );
 
-CREATE Table images (
+CREATE Table event_images (
   image_id int NOT NULL AUTO_INCREMENT,
   event_id int NOT NULL,
   image_loc varchar(255),
-  PRIMARY KEY (image_id)
+  PRIMARY KEY (image_id),
+  FOREIGN KEY (event_id) REFERENCES events (event_id) ON DELETE CASCADE
 );
 
 CREATE Table attendance (
-  att_id int NOT NULL AUTO_INCREMENT,
   user_id int NOT NULL,
   event_id int NOT NULL,
   user_status int,
-  PRIMARY KEY(att_id)
+  FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+  FOREIGN KEY (event_id) REFERENCES events(event_id) ON DELETE CASCADE
 );
 
 CREATE Table organiser (
   org_id int NOT NULL AUTO_INCREMENT,
   event_id int NOT NULL,
   user_id int NOT NULL,
-  PRIMARY KEY(org_id)
+  PRIMARY KEY(org_id),
+  FOREIGN KEY (event_id) REFERENCES events(event_id) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE Table user_logs (
   log_id int NOT NULL AUTO_INCREMENT,
   user_id int NOT NULL,
   date_time timestamp,
-  PRIMARY KEY (log_id)
+  PRIMARY KEY (log_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE NO ACTION
 );
 
 CREATE Table notifications (
@@ -85,7 +97,8 @@ CREATE Table notifications (
   user_id int NOT NULL,
   type_id int NOT NULL,
   created_at timestamp,
-  PRIMARY KEY (notification_id)
+  PRIMARY KEY (notification_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE Table notification_type (
