1b6fa3cead99079e2f4f2d6c021117d15db68a21
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-06-08 13:23:25 +0000

    Wed, Jun 8, 2022, 1:23 PM +00:00

--- Fest_Back_End/routes/User_Auth/register.js
+++ Fest_Back_End/routes/User_Auth/register.js
@@ -13,9 +13,9 @@ router.post('/register', function(req,res,next){
             return;
         }
 
-        let query = `UPDATE users SET first_name = ?, last_name = ?, dob = ?,  street = ?, city = ?, country = ?, post_code = ? WHERE user_id = ${req.session.user};`;
+        let query = `UPDATE users SET first_name = ?, last_name = ?, dob = ?,  street = ?, city = ?, country = ?, state = ?, post_code = ? WHERE user_id = ${req.session.user};`;
         console.log(req.body);
-        connection.query(query,[req.body.first_name, req.body.last_name, req.body.dob, req.body.street, req.body.city, req.body.country, req.body.post_code],function(error,rows, fields){
+        connection.query(query,[req.body.first_name, req.body.last_name, req.body.dob, req.body.street, req.body.city, req.body.country, req.body.state, req.body.post_code],function(error,rows, fields){
             connection.release();
             if(error){
                 res.sendStatus(500);
