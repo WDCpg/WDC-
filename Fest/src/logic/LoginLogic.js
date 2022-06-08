f4be92852b00433b45dbf0842158049fb40f079d
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-06-08 12:43:23 +0000

    Wed, Jun 8, 2022, 12:43 PM +00:00

--- Fest/src/logic/LoginLogic.js
+++ Fest/src/logic/LoginLogic.js
@@ -27,7 +27,8 @@ export default {
             console.log('Image URL: ' + profile.getImageUrl());
             console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 
-            googleUser.getAuthResponse().id_token;
+            var id_token = googleUser.getAuthResponse().id_token;
+            console.log("ID Token: " + id_token)
         }
     },
         signOut() {
