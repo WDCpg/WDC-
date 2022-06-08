e6863d2d23876f8f86e33af05f2a5f6e20ecca2a
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-06-08 10:58:34 +0000

    Wed, Jun 8, 2022, 10:58 AM +00:00

--- Fest/src/api/LoginsAPI.js
+++ Fest/src/api/LoginsAPI.js
@@ -13,13 +13,21 @@ export default {
                 return status([this.status, {test: 56}]);
             }
         }
-        
+
         //Open connection
         xhttp.open('POST', `http://localhost:8080/login`, true);
         xhttp.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
-        
+
         //Send request
         console.log(JSON.stringify(auth))
         xhttp.send(JSON.stringify(auth));
+    },
+    xhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            return status(this.status);
+        }
+        else if (this.readyState == 4)  {
+            return status([this.status, {test: 56}]);
+        }
     }
 }
