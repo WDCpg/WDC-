b5a8291a88136066b1a0afa3e238b0e1bce07038
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-06-08 12:58:30 +0000

    Wed, Jun 8, 2022, 12:58 PM +00:00

--- Fest/src/components/Login.vue
+++ Fest/src/components/Login.vue
@@ -31,7 +31,7 @@
                             <div class="signinbuttons">
                             <button type="button" @click="submitLogin" class="signin-button">Sign in</button><br>
                             <div class="g-signin2" data-onsuccess="onSignIn"></div>
-                            <button type="button" @click="signOut()">Sign out</button>
+                            <button type="button" onclick="signOut()">Sign out</button>
                             </div>
                         </form>
 
