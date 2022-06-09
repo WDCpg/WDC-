c2ae9b641d74c3db85789c8030722fb4a0ce9aa8
Foolfish <99941624+Foolfish93@users.noreply.github.com>
2022-06-09 12:24:07 +0000

    Thu, Jun 9, 2022, 12:24 PM +00:00

--- Fest/src/components/NewEvent.vue
+++ Fest/src/components/NewEvent.vue
@@ -162,6 +162,9 @@
                 <div class = "event-group">
                     <input type="radio" id="closeFriend" name="eventGroup" v-model="newEventData.privacy" value="Close Friend">
                     <label for="closeFriend" class="event-group-selector">
+                            </div>
+                                &#128151;
+                            <div>
                             <p>
                                 Close <br> Friends
                             </p>
