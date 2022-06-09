6699772589d4a6aacac86d3cc7698a9e677b6544
Foolfish <99941624+Foolfish93@users.noreply.github.com>
2022-06-09 12:07:01 +0000

    Thu, Jun 9, 2022, 12:07 PM +00:00

--- Fest/src/components/NewEvent.vue
+++ Fest/src/components/NewEvent.vue
@@ -70,7 +70,7 @@
                         <div >
                             <ul class = "emoji-list">
                                 <li><button class = "emoji-button">&#128513;</button></li>
-                                <li><button class = "emoji-button" title="U+263A"><span>U+263A;</span></button></li>
+                                <li><button class = "emoji-button">&#128514;</button></li>
                                 <li><button class = "emoji-button">&#128515;</button></li>
                                 <li><button class = "emoji-button">&#128516;</button></li>
                                 <li><button class = "emoji-button">&#128517;</button></li>
