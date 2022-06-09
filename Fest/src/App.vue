5129e2b497319e258f6212e7e926b78404906fb4
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-06-09 14:00:51 +0000

    Thu, Jun 9, 2022, 2:00 PM +00:00

--- Fest/src/App.vue
+++ Fest/src/App.vue
@@ -83,5 +83,15 @@ main {
   display: flex;
 }
 
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
 </style>
 
