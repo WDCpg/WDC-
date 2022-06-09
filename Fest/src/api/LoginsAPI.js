554c502c05fb3b444461f836bf6283a9f63bca5c
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-06-09 08:49:22 +0000

    Thu, Jun 9, 2022, 8:49 AM +00:00

--- Fest/src/api/LoginsAPI.js
+++ Fest/src/api/LoginsAPI.js
@@ -13,7 +13,7 @@ export default {
             }
         }
 
-        //Open connection
+        //Open connection 8080 is for back end, 3000 is for front end
         xhttp.open('POST', `https://fulzske-code50-61676631-9pr7w7wq27rwr-8080.githubpreview.dev/login`, true);
         xhttp.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
 
