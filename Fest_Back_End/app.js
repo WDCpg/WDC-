70334a6d0de648776ece7b4af16f06c890290649
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-06-09 11:26:09 +0000

    Thu, Jun 9, 2022, 11:26 AM +00:00

--- Fest_Back_End/app.js
+++ Fest_Back_End/app.js
@@ -34,17 +34,17 @@ var dbConnectionPool = mysql.createPool({host: '127.0.0.1', database: 'fest_db'}
 //   password: 'Roomdly1234',
 //   database: 'fest_db'
 // });
-//var dbConnectionPool = mysql.createPool({host: '127.0.0.1', database: 'fest_db', });
+var dbConnectionPool = mysql.createPool({host: '127.0.0.1', database: 'fest_db' });
 
 // local env 2
-var dbConnectionPool = mysql.createPool({
-  name: 'fest-db',
-  host: 'localhost',
-  port: '3306',
-  user: 'root',
-  password: 'Roomdly1234',
-  database: 'fest_db'
-});
+// var dbConnectionPool = mysql.createPool({
+//   name: 'fest-db',
+//   host: 'localhost',
+//   port: '3306',
+//   user: 'root',
+//   password: 'Roomdly1234',
+//   database: 'fest_db'
+// });
 
 
 //connect to database middleware
