e1d817fc4fe41753f2ac052ae98737cffdffa87f
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-06-09 12:13:24 +0000

    Thu, Jun 9, 2022, 12:13 PM +00:00

--- Fest/src/logic/SignupLogic.js
+++ Fest/src/logic/SignupLogic.js
@@ -7,7 +7,17 @@ export default {
         }
     },
     methods: {
+        postNewSignupInfo() {
+            const checkEmail = document.getElementById("email");
+            const checkPassword = document.getElementById("password");
+            if (checkPassword.value.length && checkEmail.value.length > 8) {
+                console.log(checkEmail);
+                console.log(checkPassword);
+                store.dispatch('postRegisterData');
+            } else {
 
+            }
+        }
     }
 }
 
