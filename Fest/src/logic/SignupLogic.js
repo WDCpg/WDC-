940c413e38e6bd5397a254670e1cb6ef40762967
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-06-09 11:45:56 +0000

    Thu, Jun 9, 2022, 11:45 AM +00:00

---
+++ Fest/src/logic/SignupLogic.js
@@ -0,0 +1,38 @@
+import store from "@/store/index";
+
+export default {
+    computed: {
+        newSignupData(){
+            return store.state.newSignupData;
+        }
+    },
+    methods: {
+        postSignupInfo() {
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
