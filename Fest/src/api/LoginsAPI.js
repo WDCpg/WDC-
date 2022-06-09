2b675d60ecafaf7907b7caffc37f7df3eb1eebe9
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-06-09 11:36:22 +0000

    Thu, Jun 9, 2022, 11:36 AM +00:00

--- Fest/src/api/LoginsAPI.js
+++ Fest/src/api/LoginsAPI.js
@@ -16,29 +16,8 @@ export default {
         //Open connection 8080 is for back end, 3000 is for front end
         xhttp.open('POST', `https://fulzske-code50-61676631-9pr7w7wq27rwr-8080.githubpreview.dev/login`, true);
         xhttp.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
-
+        xhttp.withCredentials = true;
         //Send request
         xhttp.send(JSON.stringify(auth));
     },
-
-    googleLogin (token, status) {
-        console.log(token)
-        let xhttp = new XMLHttpRequest();
-
-        xhttp.onreadystatechange = function() {
-            if(this.readyState == 4 && this.status == 200) {
-                return status(this.status);
-            }
-            else if (this.readyState == 4)  {
-                return status([this.status, {test: 56}]);
-            }
-        }
-
-        xhttp.open('POST', `https://fulzske-code50-61676631-9pr7w7wq27rwr-3000.githubpreview.dev/login`, true);
-        xhttp.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
-
-        //Send request
-        console.log(JSON.stringify(token))
-        xhttp.send(JSON.stringify({token: googleUser.getAuthResponse().id_token}));
-    }
 }
