8842f9ca6710fa9be0f5ab886d46bfe073dc2b4d
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-06-09 09:50:28 +0000

    Thu, Jun 9, 2022, 9:50 AM +00:00

--- Fest/src/main.js
+++ Fest/src/main.js
@@ -18,10 +18,10 @@ import gAuthPlugin from 'vue3-google-oauth2';
 // need to install command before google sign in will work
 //npm i vue3-google-oauth2
 
-let gauthClientId = "533508693712-r61sce51m123m23fv89sohgch0ej1r6n.apps.googleusercontent.com";
-
 /* APP ARCHITECTURE */
 const app = createApp(App);
+
+let gauthClientId = "533508693712-r61sce51m123m23fv89sohgch0ej1r6n.apps.googleusercontent.com";
 
 // Router
 app.use(router);
