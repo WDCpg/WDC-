6b4a6b21b52327a8078ff6bc7af06a3bb59aa073
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-06-10 05:31:40 +0000

    Fri, Jun 10, 2022, 5:31 AM +00:00

--- Fest/src/components/Test.vue
+++ Fest/src/components/Test.vue
@@ -1,21 +1,23 @@
 <template>
 <div class="g-signin2" data-onsuccess="onSignIn()"></div>
 
-<div id="content"></div>
-<button onclick="signOut()">Sign out</button>
+
 
 </template>
 <script>
-  function onSignIn(googleUser) {
-    console.log('WORKING');
-    console.log('user is ' +JSON.stringify(googleUser.getBasicProfile()));
 
-    function signOut() {
-      gapi.auth2.getAuthInstance().signOut().then(function() {
-        console.log("User signed out")
-        })   
-    }
+export default {
+  methods: {
+      onSignIn(googleUser) {
+      console.log('WORKING');
+      console.log('user is ' +JSON.stringify(googleUser.getBasicProfile()));
+     }
+
+
+
 }
+
+
 
 </script>
 
