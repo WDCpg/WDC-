e4c65cb078d4fa6e4787457ffb820195fbaef6b5
Foolfish <99941624+Foolfish93@users.noreply.github.com>
2022-06-10 08:18:57 +0000

    Fri, Jun 10, 2022, 8:18 AM +00:00

--- Fest_Back_End/routes/newEvent/newEvent.js
+++ Fest_Back_End/routes/newEvent/newEvent.js
@@ -22,41 +22,41 @@ var router = express.Router();
 //     });
 // })
 
-// var path = require('path');
-// var multer = require('multer');
-// var storage = multer.diskStorage({
-//     destination: (req,file,cb) => {
-//         cb(null, '../Fest/src/assets/cover')
-//     },
-//     filename:(req,file,cb) => {
-//         console.log(file)
-//         cb(null, Date.now() + path.extname(file.originalname));
-//     },
-// });
+var path = require('path');
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: (req,file,cb) => {
+        cb(null, '../Fest/src/assets/cover')
+    },
+    filename:(req,file,cb) => {
+        console.log(file)
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+});
 
-// var upload = multer({storage: storage});
+var upload = multer({storage: storage});
 
-// router.post('/uploadCoverPhoto', upload.array('file', 12));
+router.post('/uploadCoverPhoto', upload.array('file', 12));
 
-// router.post('/newEvent', function(req,res,next){
-//     req.db.getConnection(function(error, connection){
-//         if (error){
-//             console.log(error);
-//             res.sendStatus(500);
-//             return;
-//         }
-//         console.log(req.files);
-//             let query = "UPDATE users SET profile_picture=? WHERE user_id = ?;";
-//             connection.query(query,[file.filename,], function(error, rows, fields){
-//                 connection.release();
-//                 if(error){
-//                     res.sendStatus(500);
-//                     return;
-//                 }
-//                 res.send("success");
-//             });
-//     });
-// });
+router.post('/newEvent', function(req,res,next){
+    req.db.getConnection(function(error, connection){
+        if (error){
+            console.log(error);
+            res.sendStatus(500);
+            return;
+        }
+        console.log(req.files);
+            let query = "UPDATE users SET profile_picture=? WHERE user_id = ?;";
+            connection.query(query,[file.filename,], function(error, rows, fields){
+                connection.release();
+                if(error){
+                    res.sendStatus(500);
+                    return;
+                }
+                res.send("success");
+            });
+    });
+});
 
 router.post('/newEvent', function(req, res, next) {
     req.db.getConnection(function(error, connection){
