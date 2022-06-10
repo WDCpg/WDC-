5e59d11d726f5b5ad3ccde3b4853b9429493f5fb
Foolfish <99941624+Foolfish93@users.noreply.github.com>
2022-06-10 08:11:29 +0000

    Fri, Jun 10, 2022, 8:11 AM +00:00

--- Fest/src/components/NewEvent.vue
+++ Fest/src/components/NewEvent.vue
@@ -53,7 +53,7 @@
                     <div class = "emoji">
                         <div class="emoji-search-container">
                             <form class = "emoji-search">
-                                <input v-model="newEventData.icon" class = "emoji-search-text" type="search" placeholder="Search Emoji...Coming Soon..." name ="emoji">
+                                <input class = "emoji-search-text" type="search" placeholder="Search Emoji...Coming Soon..." name ="emoji">
                                 <svg class = "emoji-search-button" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg>
                             </form>
 
