6ec4c74dc3bb3a8e9ff2cbd169f3cfe14655ed6e
Foolfish <99941624+Foolfish93@users.noreply.github.com>
2022-06-10 08:57:14 +0000

    Fri, Jun 10, 2022, 8:57 AM +00:00

--- Fest/src/api/NewEventAPI.js
+++ Fest/src/api/NewEventAPI.js
@@ -12,7 +12,7 @@ export default {
         }
         //Open connection
         xhttp.open('POST', `https://foolfish93-code50-99941624-97xg6q9xq3794w-8080.githubpreview.dev/newEvent`, true);
-        xhttp.setRequestHeader('Content-type', 'application/json; charset=UTF-8' );
+        //xhttp.setRequestHeader('Content-type', 'application/json; charset=UTF-8' );
         //Send request
         xhttp.send(JSON.stringify(newEventData));
     }
