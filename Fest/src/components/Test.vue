c2ef84687ac9f80c1094e1991399546c2382be83
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-06-10 07:43:01 +0000

    Fri, Jun 10, 2022, 7:43 AM +00:00

--- Fest/src/components/Test.vue
+++ Fest/src/components/Test.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <div id="google-signin-button"></div>
+  <div id="google-signin-button"</div>
   <button @click="signOut">Sign Out</button>
   <div> {{ googleData }} </div>
 </template>
