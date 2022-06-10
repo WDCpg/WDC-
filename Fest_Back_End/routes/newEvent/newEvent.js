904b3856166c4e79748fb23997729ddd572b8994
Foolfish <99941624+Foolfish93@users.noreply.github.com>
2022-06-10 06:07:36 +0000

    Fri, Jun 10, 2022, 6:07 AM +00:00

--- Fest_Back_End/routes/newEvent/newEvent.js
+++ Fest_Back_End/routes/newEvent/newEvent.js
@@ -66,7 +66,7 @@ router.post('/newEvent', function(req, res, next) {
             return;
         }
 
-        let query = "INSERT INTO users (first_name, last_name, email, contact_number, city, street, country, post_code) VALUES (?,?,?,?,?,?,?,?) where user_id = ?;";
+        let query = "INSERT INTO event (event_start, event_end, event_description, event_title, country, state, city, street, post_code) VALUES (?,?,?,?,?,?,?,?) where user_id = ?;";
         connection.query(query,[req.body.first_name, req.body.last_name, req.body.email, req.body.contact_number, req.body.city, req.body.street, req.body.country, req.body.post_code, req.session.userId],function(error,rows, fields){
             connection.release();
             if(error){
