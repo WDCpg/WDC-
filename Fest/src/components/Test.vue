162459f7ceef18e24685d0242c5e2b0b2e10abf6
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-06-10 07:55:15 +0000

    Fri, Jun 10, 2022, 7:55 AM +00:00

--- Fest/src/components/Test.vue
+++ Fest/src/components/Test.vue
@@ -1,7 +1,6 @@
 
 <template>
-  <div id="google-signin-button"</div>
-  <button @click="signOut">Sign Out</button>
+
 </template>
 
 <script src="@/logic/TestLogic.js"></script>
--- Fest/src/logic/TestLogic.js
+++ Fest/src/logic/TestLogic.js
@@ -25,6 +25,7 @@ export default {
               console.log('User signed out')
                 }))
           }
+        
     }
 }
 
