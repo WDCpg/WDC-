22bfddd92d65087feb720ea64926093081bd4ca8
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-06-10 08:36:24 +0000

    Fri, Jun 10, 2022, 8:36 AM +00:00

--- Fest/src/components/Test.vue
+++ Fest/src/components/Test.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="g-signin2" data-onsuccess="onSignIn"></div>
-<button @click="signOut()"</button>
+<button @click="signOut()">Sign Out</button>
 </template>
 
 <script src="@/logic/TestLogic.js"></script>
