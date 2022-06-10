e9d84060062fdea8a1ffa70554afce5ba7c4c61a
Foolfish <99941624+Foolfish93@users.noreply.github.com>
2022-06-10 05:36:34 +0000

    Fri, Jun 10, 2022, 5:36 AM +00:00

--- Fest/src/api/NewEventAPI.js
+++ Fest/src/api/NewEventAPI.js
@@ -1,6 +1,20 @@
 export default {
     postNewEvent(newEventData){
         console.log(newEventData)
+
+        let xhttp = new XMLHttpRequest();
+
+        //Run on response
+        xhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            return this.response;
+            }
+        }
+        //Open connection
+        xhttp.open('POST', `http://localhost:8080/newEvent`, true);
+        xhttp.setRequestHeader('Content-type', 'application/json; charset=UTF-8' );
+        //Send request
+        xhttp.send(JSON.stringify(newUserData));
     },
 
     postFriendInvited(friendInfo) {
