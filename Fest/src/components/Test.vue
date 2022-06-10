4dd90d269e9d62c6bcf0a4c33ff98def5fad9639
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-06-10 06:40:40 +0000

    Fri, Jun 10, 2022, 6:40 AM +00:00

--- Fest/src/components/Test.vue
+++ Fest/src/components/Test.vue
@@ -1,3 +1,22 @@
+
 <template>
-    
+  <div id="google-signin-button"></div>
 </template>
+
+<script>
+export default {
+  mounted() {
+    gapi.signin2.render('google-signin-button', {
+      onsuccess: this.onSignIn
+    })
+  },
+  methods: {
+    onSignIn (user) {
+      const profile = user.getBasicProfile()
+      const myuser = user
+      console.log(profile)
+      console.log(myuser)
+    }
+  }
+}
+</script>
