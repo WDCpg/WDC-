83454687cef22d471b2f6b4ed70061134fcbc701
Fulzske <61676631+Fulzske@users.noreply.github.com>
2022-06-11 04:51:42 +0000

    Sat, Jun 11, 2022, 4:51 AM +00:00

--- Fest/src/main.js
+++ Fest/src/main.js
@@ -17,15 +17,6 @@ import Test from './components/Test.vue';
 /* APP ARCHITECTURE */
 const app = createApp(App);
 
-// import gAuthPlugin from 'vue3-google-oauth2';
-// let gauthClientId = '676785461988-icoil0dtlld2fcp5kb22llst7t94mans.apps.googleusercontent.com';
-// app.use(gAuthPlugin, {
-//   clientId: gauthClientId,
-//   scope: 'email',
-//   prompt: 'consent'
-// })
-
-
 // Router
 app.use(router);
 
